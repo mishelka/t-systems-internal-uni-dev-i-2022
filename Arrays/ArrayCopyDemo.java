3a675faef6f130582a5f1f162802b34ceef977a3
Michaela Bačíková <bacikova.michaela@gmail.com>
2022-07-08 12:55:54 +0200

    array copy priklad s meranim casu

--- Arrays/ArrayCopyDemo.java
+++ Arrays/ArrayCopyDemo.java
@@ -1,25 +1,66 @@
 public class ArrayCopyDemo {
+    private static long startTime;
+    private static long endTime;
+    private static int iteration = 0;
+
     public static void main(String[] args) {
-        int[] copyFrom = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 133, 14};
-        int[] copyTo = new int[7];
-        int[] copyFrom2 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 133, 14};
-        int[] copyTo2 = new int[7];
+        int[] copyFrom = new int[11000];
+        int[] copyTo = new int[11000];
+        int[] copyFrom2 = new int[11000];
+        int[] copyTo2 = new int[11000];
 
-        System.arraycopy(copyFrom, 2, copyTo, 0, 7);
-        for(int i : copyTo) {
-            System.out.print(i + ",");
+        //populate array
+        for (int i = 0; i < 1000; i++) {
+            copyFrom[i] = i + 10;
+            copyTo[i] = 0;
+            copyFrom[i] = i + 10;
+            copyTo[i] = 0;
         }
-        System.out.println();
 
-        arraycopy(copyFrom2, 2, copyTo2, 0, 7);
-        for(int i : copyTo) {
-            System.out.print(i + ",");
-        }
-        System.out.println();
+        recordStart();
+        System.arraycopy(copyFrom, 2, copyTo, 0, 10000);
+        recordAndWriteEnd();
+
+//        for(int i : copyTo) {
+//            System.out.print(i + ",");
+//        }
+//        System.out.println();
+
+        recordStart();
+        arraycopy(copyFrom2, 2, copyTo2, 4, 10000);
+        recordAndWriteEnd();
+
+//        for(int i : copyTo) {
+//            System.out.print(i + ",");
+//        }
+//        System.out.println();
     }
 
     private static boolean arraycopy(int[] source, int srcIndex,
                                      int[] dest, int destIndex, int length) {
-        return false;
+        if (srcIndex + length >= source.length
+                || destIndex + length >= dest.length) {
+            return false;
+        }
+        String abc = "";
+        for (int i = 0; i < length; i++) {
+            dest[destIndex + i] = source[srcIndex + i];
+
+            //!!! nie !!!
+            //abc += dest[destIndex + 1];
+            //System.out.println(dest[destIndex + 1]);
+        }
+        System.out.println(abc);
+        return true;
+    }
+
+    private static void recordStart() {
+        startTime = System.currentTimeMillis();
+    }
+
+    private static void recordAndWriteEnd() {
+        endTime = System.currentTimeMillis();
+        System.out.println("Loop " + ++iteration + " duration: "
+                + (endTime - startTime) + " milliseconds");
     }
 }
