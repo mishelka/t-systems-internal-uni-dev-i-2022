27ccdccf357b3a47b5e487ddef9e60b332b16517
Michaela Bačíková <bacikova.michaela@gmail.com>
2022-07-21 13:27:49 +0200

    uloha domaca na files

--- IO/io/ListDir.java
+++ IO/io/ListDir.java
@@ -1,20 +1,55 @@
 package io;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class ListDir {
     public static void main(String[] args) {
-        File dir = new File(".");
+        String path = ".";
+        listDir(new File(path));
+        listDir(path);
 
-        if (dir.isDirectory()) {
-            System.out.println("Listing of: " + dir.getAbsolutePath());
+        List<File> filteredByName = filter("./arrays", "Array");
+        List<File> filteredByDate = filter("./arrays", new Date());
+    }
 
-            //List directory
-            for (String fileName : dir.list()) {
-                System.out.println("  " + fileName);
+    private static void listDir(String fileName) {
+        listDir(new File(fileName));
+    }
+
+    private static void list(List<File> files) {
+        //vypisat zoznam suborov
+        //pouzit stream a forEach
+    }
+    private static void listDir(File f) {
+        System.out.println("  " + f.getName());
+        if (f.isDirectory()) {
+            for(File sub : f.listFiles()) {
+                listDir(sub);
             }
-        } else {
-            System.err.printf("File %s is not directory", dir);
         }
+    }
+
+    private static List<File> filter(String path, String name) {
+        List<File> filtered = new ArrayList<File>();
+        File dir = new File(name);
+        filter(dir, filtered);
+        return filtered;
+    }
+
+    private static void filter(File f, List<File> filtered) {
+//        if(podmienka) {
+//            filtered.add(f);
+//        }
+//        if(f.isDirectory()) {
+//            //for(subf) filter(subf, filtered);
+//        }
+    }
+
+    private static List<File> filter(String path, Date minDate) {
+//        f.lastModified() - vrati long
+        return null;
     }
 }
